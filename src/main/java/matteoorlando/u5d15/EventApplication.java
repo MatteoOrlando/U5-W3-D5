73d76d53a30f487866345debd31449a48f3500d9
MatteoOrlando <mat.orlando88@gmail.com>
2024-04-19 13:18:54 +0200

    main basic
    Commit di base con main, incluso di pom.xml aggiornato e le directory giá preparate per le branch.

---
+++ src/main/java/matteoorlando/u5d15/EventApplication.java
@@ -0,0 +1,7 @@
+package matteoorlando.u5d15;
+
+public class EventApplication {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/matteoorlando/u5d15/EventApplicationTest.java
@@ -0,0 +1,13 @@
+package matteoorlando.u5d15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EventApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
