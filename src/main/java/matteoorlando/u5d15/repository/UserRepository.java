91799921135ffd839b61c6ad27932e153eaa111d
MatteoOrlando <mat.orlando88@gmail.com>
2024-04-19 14:09:17 +0200

    Update UserRepository.java

--- src/main/java/matteoorlando/u5d15/repository/UserRepository.java
+++ src/main/java/matteoorlando/u5d15/repository/UserRepository.java
@@ -3,11 +3,10 @@ package matteoorlando.u5d15.repository;
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    Object save(User user);
-    void update(User user);
-    void delete(User user);
+    List<User> findByRoles_Name(String roleName);
 }
